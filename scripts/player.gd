aa48037136bcb0f10ef7a89f0685a691741ef5df
victorvhs <victor.h.s.reis@gmail.com>
2018-12-12 11:40:14 -0200

    Parallax

---
+++ scripts/player.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+const SPEED = 750
+const GRAVITY = 3600
+const UP = Vector2(0,-1)
+const JUMP_SPEED = -1750
+var motion = Vector2()
+
+func _physics_process(delta):
+	update_motion(delta)
+	
+func update_motion(delta):
+	fall(delta)
+	run()
+	jump()
+	move_and_slide(motion,UP)
+
+func _process(delta):
+	update_animation(motion)
+	
+func update_animation(motion):
+	$AnimatedSprite.update(motion)
+	
+func fall(delta):
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY * delta	
+
+func jump():
+	if Input.is_action_pressed("ui_up") and is_on_floor():
+		motion.y = JUMP_SPEED
+
+func run():
+	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
