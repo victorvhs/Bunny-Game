bf54f2364c079fd6a0663e7a450e617445cf1ae0
victorvhs <victor.h.s.reis@gmail.com>
2018-12-17 14:55:09 -0200

    life

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+export var start_live = 3
+var lives
 func _ready():
 	Global.GameState = self
-	
+	lives = start_live
+
 func end_game():
 	get_tree().change_scene("res://scenes/gameOver.tscn")
